c867ef284d2ac48674127e5c46536c8ff04b1fe7
maisunmonowar <maisunmonwar@gmail.com>
2023-01-23 12:22:27 +0900

    cleanup

--- msvscpp/udp_client/u.h
+++ msvscpp/udp_client/u.h
@@ -1,22 +1,21 @@
 #ifndef __U_H__
 #define __U_H__
 
-#include <comdef.h>  // you will need this
+#include <comdef.h> // you will need this
 #include <string>
 
 class MySocketClass
 {
 public:
-
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "7001"
 #define INFO_BUFFER_SIZE 32767
 
-	TCHAR  infoBuf[INFO_BUFFER_SIZE] = { 0 };
-	DWORD  bufCharCount = INFO_BUFFER_SIZE;
+	TCHAR infoBuf[INFO_BUFFER_SIZE] = {0};
+	DWORD bufCharCount = INFO_BUFFER_SIZE;
 	SOCKET ConnectSocket = INVALID_SOCKET;
 	std::string termination_string = "terminate";
-	char recvbuf[DEFAULT_BUFLEN] = { 0 };
+	char recvbuf[DEFAULT_BUFLEN] = {0};
 	int recvbuflen = DEFAULT_BUFLEN;
 	int iResult;
 	enum udp_error_status
@@ -26,11 +25,11 @@ public:
 		wsa_startup_error,
 		addrinfo_error
 	};
-	udp_error_status error_status_self = all_ok; // If the init wasn't successfull, this will change. 
+	udp_error_status error_status_self = all_ok; // If the init wasn't successfull, this will change.
 
 	MySocketClass();
 	~MySocketClass();
-	int sendData(char* ourMessage, int ourMessage_size);
+	int sendData(char *ourMessage, int ourMessage_size);
 	bool receiveSomething();
 
 private:
