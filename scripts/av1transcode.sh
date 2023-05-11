7076ffd50bc58dd22eada022d211d84b7479786b
maisunmonowar <maisunmonwar@gmail.com>
2023-05-11 18:29:36 +0900

    somewhat works.

--- scripts/av1transcode.sh
+++ scripts/av1transcode.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-INPUT_DIR = "$PWD"
-OUTPUT_DIR = "$PWD"
+# INPUT_DIR = "$PWD"
+# OUTPUT_DIR = "$PWD"
 
-for file in *$INPUT_DIR"/*.{mp4,mkv,avi}; do 
-    filename = $(basename "$file")
-    echo "Transcoding $filename to AV1..."
-    ffmpeg -i "$file" -c:v libaom-av1 -crf 19 -b:v 0 -c:a copy "$OUTPUT_DIR/${filename%.*}.webm"
+for file in *.avi; do 
+    echo "Transcoding $file to AV1..."
+    ffmpeg -i "$file" -c:v libaom-av1 -crf 19 -b:v 0 -c:a copy "${file%.*}_av1.mp4"
 done 
 
 echo "Transcoding done."
